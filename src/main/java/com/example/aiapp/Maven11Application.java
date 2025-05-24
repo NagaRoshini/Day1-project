f88dac56b279f277d8227df732001b404013b52d
NagaRoshini <nagaroshini7@gmail.com>
2025-05-24 14:16:23 +0530

    Initial commit

---
+++ src/main/java/com/example/aiapp/Maven11Application.java
@@ -0,0 +1,13 @@
+package com.example.aiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Maven11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Maven11Application.class, args);
+    }
+
+}
